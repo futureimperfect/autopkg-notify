0fb1cce41591cd6b779d5bd4cdc51f7fa9f1cea5
futureimperfect <james@everythingisgray.com>
2014-04-08 17:44:19 -0700

    Started fleshing out the AutoPkgNotify script. Haven't tested anything yet.

--- autopkg_notify_conf.py
+++ autopkg_notify_conf.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 # This is a sample configuration file
-# for the AutoPkg-Notify Script.
+# for the AutoPkgNotify Script.
 
 #### SMTP CONFIG ####
-smtp_user = 'you@you.com'
-smtp_pass = 'supersecretpassword'
-smtp_port = 587
-smtp_tls  = True
+smtp_from   = 'you@you.com'
+smtp_to     = ['me@me.com', 'you@you.com']
+smtp_pass   = 'supersecretpassword' # TODO: Don't use plain text, use `with open()`
+smtp_port   = 587
+smtp_server = 'smtp.gmail.com'
+smtp_user   = 'you@you.com'
+smtp_tls    = True
 
 #### AutoPkg Recipes to Run ####
 recipes_to_run = ['AdobeAir.jss', 'AdobeFlashPlayer.jss', 'AdobeReader.jss', 'Firefox.jss', 'GoogleEarth.jss', 'MSOffice2011Updates.jss', 'Skype.jss', 'TextWrangler.jss']
