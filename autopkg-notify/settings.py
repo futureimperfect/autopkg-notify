8c4d2c7a24c9d1b85706c21bd3d5d31bfffa0cb1
futureimperfect <james@everythingisgray.com>
2014-04-22 18:34:30 -0700

    Improved logging and added some more docstrings.

--- autopkg-notify/settings.py
+++ autopkg-notify/settings.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python
+#
+# AutoPkgNotify - Email notifications for AutoPkg
+# Copyright 2014 James Barclay
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Sample configuration file for AutoPkgNotify
 
-# Sample configuration file for AutoPkgNotify.
+import os
 
 #### SMTP CONFIG ####
-SMTP_FROM   = 'you@you.com'
+SMTP_FROM   = 'autopkg-notify@you.com'
 SMTP_TO     = ['me@me.com', 'you@you.com']
 SMTP_PORT   = 587
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_USER   = 'you@you.com' # Leave blank if auth isn't required, (e.g., '')
 SMTP_TLS    = True
 
-#### AutoPkg Recipes to Run ####
-RECIPES_TO_RUN = ['AdobeAir.jss', 'AdobeFlashPlayer.jss', 'AdobeReader.jss', 'Firefox.jss', 'GoogleEarth.jss', 'MSOffice2011Updates.jss', 'Skype.jss', 'TextWrangler.jss']
+#### LOG CONFIG ####
+LOG_DIR  = '/Users/Shared'
+LOG_FILE = os.path.join(LOG_DIR, 'autopkg-notify.log')
 
-#### SMTP PASSWORD SETTINGS (DO NOT MODIFY) ####
-#### Include plain text password in p.txt if auth is required ####
-import os
+#### AUTOPKG RECIPES TO RUN ####
+RECIPES_TO_RUN = (
+    'AdobeAir.jss',
+    'AdobeFlashPlayer.jss',
+    'AdobeReader.jss',
+    'Firefox.jss',
+    'GoogleEarth.jss',
+    'MSOffice2011Updates.jss',
+    'Skype.jss',
+    'TextWrangler.jss'
+)
+
+#### SMTP PASSWORD SETTINGS ####
+#### INCLUDE PLAIN TEXT PASSWORD IN P.TXT IF AUTH IS REQUIRED ####
 SMTP_PASS = None
 PW = '../p.txt'
 if os.path.isfile(PW):
